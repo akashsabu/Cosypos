68021389eabe7d9289277765a1fe04d13f7d78ce
akashsabu <akashpaulmsabu1999@gmail.com>
2023-08-06 18:28:40 +0530

    cart item completed

--- src/components/modules/Billing.js
+++ src/components/modules/Billing.js
@@ -1,33 +1,47 @@
-import React from 'react'
+import React from "react";
 import Cart from "./Cart";
-import styled from  "styled-components"; 
+import styled from "styled-components";
 
 export default function Billing({ items, onIncrement, onDecrement }) {
   return (
-
     <BillingContainer>
-    <Cart
-    items={items}
-    onIncrement={onIncrement}
-    onDecrement={onDecrement}
-  /> 
+      <CartContainer>
+        <Cart
+          items={items}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+        />
+      </CartContainer>
 
 
 
 
-  <div className="total">
-        Total: ${items.reduce((acc, item) => acc + item.count * item.price, 0).toFixed(2)}
+      <div className="total">
+        Total: $
+        {items
+          .reduce((acc, item) => acc + item.count * item.price, 0)
+          .toFixed(2)}
       </div>
-  </BillingContainer>
-
-   )
+    </BillingContainer>
+  );
 }
 const BillingContainer = styled.div`
   width: 360px;
   height: 100%;
   /* overflow: hidden; */
-  background-color: #775;
+  background-color: #000;
   position: fixed;
   right: 0px;
   top: 0%;
+`;
+const CartContainer = styled.div`
+    height: 200px;
+    width: 300px;
+    margin: 20px auto;
+    color: white;
+    overflow-y: auto;
+    background: #000;
+    /* padding: 5px; */
+
+
 `;
--- src/components/modules/CartItem.js
+++ src/components/modules/CartItem.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 const CartItem = ({ item, onIncrement, onDecrement }) => {
   return (
-    <div className="cart-item">
-      <span>{item.name}</span>
-      <button onClick={() => onDecrement(item.id)}>-</button>
-      <span>{item.count}</span>
-      <button onClick={() => onIncrement(item.id)}>+</button>
-      
-      <span>${item.price.toFixed(2)}</span>
-      <span>Total: ${(item.count * item.price).toFixed(2)}</span>
-    </div>
+    <CartItemDiv>
+      <ItemName>{item.name}{" "}
+      <ItemCount> x{item.count}</ItemCount>
+      </ItemName>
+     
+
+      <span>${(item.count * item.price).toFixed(2)}</span>
+    </CartItemDiv>
   );
 };
 
 export default CartItem;
+
+const CartItemDiv = styled.div`
+  height: 50px;
+  width: 295px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #2D2D2D;
+  border-radius: 10px;
+  box-sizing: border-box;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+const ItemName = styled.p`
+
+`;
+const ItemCount = styled.span`
+  font-size: 14px;
+  color: #8E8E8E;
+`;
+
+
+
+// 747474
