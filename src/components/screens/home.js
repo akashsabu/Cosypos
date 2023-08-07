7decf2850730b2759d0b05014a85fcf90c0aa62e
akashsabu <akashpaulmsabu1999@gmail.com>
2023-08-07 14:46:10 +0530

    home page completed

--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import {Helmet} from "react-helmet";
+import  styled  from "styled-components"
 
 export default function home() {
   return (
-  <>
-  <Helmet>
-                <title>CosyPos | Home</title>
-            </Helmet>
-  <div>home................................................................................................................home</div>
-
-  </>
-  )
+    <Div>
+    <Helmet>
+      <title>CosyPOS | Home</title>
+    </Helmet>
+ 
+ <main class="main">
+ <section class="contact">
+   <h1 class="title"> Welcome to CosyPOS </h1>
+   <h2 class="sub-title">The fastest way to grow your business with Technology</h2>
+ </section>
+</main>
+</Div>
+);
 }
+const Div = styled.div`
+position: fixed;
+top: 0;
+overflow: hidden;
+left: 250px;
+height: 100%;
+width: 100vw;
+color: #ecf0f1;
+background: radial-gradient(ellipse at bottom, #111 0%, #090a0f 100%);
+`;
